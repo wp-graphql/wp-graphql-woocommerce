463fed44f8b59ebeb80f4dcd12150ef99a34c585
Geoff Taylor <geoffrey.taylor@outlook.com>
2019-03-19 20:39:43 -0400

    enumerations added

--- wp-graphql-woocommerce.php
+++ wp-graphql-woocommerce.php
@@ -35,8 +35,6 @@ if ( file_exists( __DIR__ . '/c3.php' ) ) {
 	require_once( __DIR__ . '/c3.php' );
 }
 
-define( 'GRAPHQL_DEBUG', true );
-
 if ( ! class_exists( '\WPGraphQL\Extensions\WPGraphQLWooCommerce' ) ) :
 		final class WPGraphQLWooCommerce {
 			/**
