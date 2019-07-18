747eb9cebd8aa22b201fe1cfff043be9b8aa4601
Geoff Taylor <kidunot89@gmail.com>
2019-07-18 12:01:12 -0400

    README.md and project version updated.

--- wp-graphql-woocommerce.php
+++ wp-graphql-woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP GraphQL WooCommerce
  * Plugin URI: https://github.com/kidunot89/wp-graphql-woocommerce
  * Description: Adds Woocommerce Functionality to WPGraphQL schema.
- * Version: 0.2.0
+ * Version: 0.2.1
  * Author: kidunot89
  * Author URI: https://axistaylor.com
  * Text Domain: wp-graphql-woocommerce
