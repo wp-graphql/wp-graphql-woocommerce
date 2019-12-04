cfe61bfc55544f33940df703d71c33a31e357c27
Geoff Taylor <kidunot89@gmail.com>
2019-12-03 19:31:53 -0500

    "ProductTaxonomyEnum" updated.

--- includes/type/enum/class-product-taxonomy.php
+++ includes/type/enum/class-product-taxonomy.php
@@ -18,19 +18,13 @@ class Product_Taxonomy {
 	 * Registers type
 	 */
 	public static function register() {
-		// Get values from attributes taxonomies.
-		$attribute_values = array();
-		foreach ( wc_get_attribute_taxonomy_names() as $attribute ) {
-			$attribute_values[ WPEnumType::get_safe_name( $attribute ) ] = array( 'value' => $attribute );
-		}
-
 		// Get values from taxonomies connected to products.
 		$taxonomy_values    = array();
 		$allowed_taxonomies = \WPGraphQL::get_allowed_taxonomies();
 		if ( ! empty( $allowed_taxonomies && is_array( $allowed_taxonomies ) ) ) {
 			foreach ( $allowed_taxonomies as $taxonomy ) {
 				$tax_object = get_taxonomy( $taxonomy );
-				if ( in_array( $post_type, $tax_object->object_type, true ) ) {
+				if ( in_array( 'product', $tax_object->object_type, true ) ) {
 					$taxonomy_values[ WPEnumType::get_safe_name( $tax_object->graphql_single_name ) ] = array( 'value' => $taxonomy );
 				}
 			}
@@ -41,7 +35,6 @@ class Product_Taxonomy {
 			array(
 				'description' => __( 'Product taxonomies', 'wp-graphql-woocommerce' ),
 				'values'      => array_merge(
-					$attribute_values,
 					$taxonomy_values,
 				),
 			)
