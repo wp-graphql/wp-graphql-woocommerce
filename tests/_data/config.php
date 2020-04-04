cb0383b5727db6fbccb58ce458e038cda6c76c5a
Geoff Taylor <kidunot89@gmail.com>
2020-04-03 21:40:17 -0400

    Stripe payment gateway support implemented

--- tests/_data/config.php
+++ tests/_data/config.php
@@ -7,3 +7,8 @@
 define( 'GRAPHQL_DEBUG', true );
 define( 'WPGRAPHQL_WOOCOMMERCE_AUTOLOAD', getenv( 'WPGRAPHQL_WOOCOMMERCE_AUTOLOAD' ) );
 define( 'GRAPHQL_JWT_AUTH_SECRET_KEY', 'testingtesting123' );
+<<<<<<< HEAD
+=======
+define( 'STRIPE_API_PUBLISHABLE_KEY', getenv( 'STRIPE_API_PUBLISHABLE_KEY' ) );
+define( 'STRIPE_API_SECRET_KEY', getenv( 'STRIPE_API_SECRET_KEY' ) );
+>>>>>>> Stripe payment gateway support implemented
