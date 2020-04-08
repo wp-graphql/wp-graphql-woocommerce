40f4dea7bcf1eb914afe6deb5051f0ccca25b745
Geoff Taylor <kidunot89@gmail.com>
2020-04-08 17:31:11 -0400

    composer.json updated.

--- tests/_data/config.php
+++ tests/_data/config.php
@@ -4,8 +4,22 @@
  * suite already bootstraps the autoloader and creates
  * fatal errors when the autoloader is loaded twice
  */
-define( 'GRAPHQL_DEBUG', true );
-define( 'WPGRAPHQL_WOOCOMMERCE_AUTOLOAD', getenv( 'WPGRAPHQL_WOOCOMMERCE_AUTOLOAD' ) );
-define( 'GRAPHQL_JWT_AUTH_SECRET_KEY', 'testingtesting123' );
-define( 'STRIPE_API_PUBLISHABLE_KEY', getenv( 'STRIPE_API_PUBLISHABLE_KEY' ) );
-define( 'STRIPE_API_SECRET_KEY', getenv( 'STRIPE_API_SECRET_KEY' ) );
+if ( ! defined( 'GRAPHQL_DEBUG' ) ) {
+    define( 'GRAPHQL_DEBUG', true );
+}
+
+if ( ! defined( 'WPGRAPHQL_WOOCOMMERCE_AUTOLOAD' ) ) {
+    define( 'WPGRAPHQL_WOOCOMMERCE_AUTOLOAD', getenv( 'WPGRAPHQL_WOOCOMMERCE_AUTOLOAD' ) );
+}
+
+if ( ! defined( 'GRAPHQL_JWT_AUTH_SECRET_KEY' ) ) {
+    define( 'GRAPHQL_JWT_AUTH_SECRET_KEY', getenv( 'GRAPHQL_JWT_AUTH_SECRET_KEY' ) );
+}
+
+if ( ! defined( 'STRIPE_API_PUBLISHABLE_KEY' ) ) {
+    define( 'STRIPE_API_PUBLISHABLE_KEY', getenv( 'STRIPE_API_PUBLISHABLE_KEY' ) );
+}
+
+if ( ! defined( 'STRIPE_API_SECRET_KEY' ) ) {
+    define( 'STRIPE_API_SECRET_KEY', getenv( 'STRIPE_API_SECRET_KEY' ) );
+}
