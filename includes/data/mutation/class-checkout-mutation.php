ce37b71514f09dd8a8fd2fc0fdfc795a4715a5e9
HW, Siew <hw.siew@actiweb.co>
2020-06-05 16:47:37 +0800

    Fixed : incorrect namespace for WC_Customer

--- includes/data/mutation/class-checkout-mutation.php
+++ includes/data/mutation/class-checkout-mutation.php
@@ -666,9 +666,9 @@ class Checkout_Mutation {
 		if ( is_user_logged_in() ) {
 			// Load customer object, but keep it cached to avoid reloading it multiple times.
 			if ( is_null( self::$logged_in_customer ) ) {
-				self::$logged_in_customer = new WC_Customer( get_current_user_id(), true );
+				self::$logged_in_customer = new \WC_Customer( get_current_user_id(), true );
 			}
-			$customer_object = new WC_Customer( get_current_user_id(), true );
+			$customer_object = new \WC_Customer( get_current_user_id(), true );
 		}
 
 		if ( ! $customer_object ) {
