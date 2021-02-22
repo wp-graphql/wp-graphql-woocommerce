dab27cd1b2b82f5ead2a8451b2763ec738c50d53
Jacob Arriola <jacobarriola@users.noreply.github.com>
2021-02-22 10:07:05 -0800

    Guard against null image_id
    We cannot assume that $source->image_id will always be available. When it's null, the corresponding resolve_post_object will throw an error because it expects an int type as the first argument, not null

--- includes/type/object/class-product-variation-type.php
+++ includes/type/object/class-product-variation-type.php
@@ -232,8 +232,7 @@ class Product_Variation_Type {
 						'type'        => 'MediaItem',
 						'description' => __( 'Product variation main image', 'wp-graphql-woocommerce' ),
 						'resolve'     => function( $source, array $args, AppContext $context ) {
-							// @codingStandardsIgnoreLine
-							return DataSource::resolve_post_object( $source->image_id, $context );
+							return ! empty( $source->image_id ) ? DataSource::resolve_post_object( $source->image_id, $context ) : null;
 						},
 					),
 				),
