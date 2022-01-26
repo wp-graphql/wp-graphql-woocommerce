7c1a7c676ef715468ac3b3a198403549bc3498b3
victormattosvm <info@victormattos.dev>
2022-01-25 23:35:57 -0300

    [Fix] Array key 'discount_type'

--- includes/mutation/class-coupon-create.php
+++ includes/mutation/class-coupon-create.php
@@ -210,8 +210,8 @@ class Coupon_Create {
 					$coupon->set_description( wp_filter_post_kses( $value ) );
 					break;
 				case 'amount':
-					if ( $coupon_args['discountType'] && 'PERCENT' === $coupon_args['discountType'] ) {
-						$coupon->set_discount_type( 'PERCENT' );
+					if ( $coupon_args['discount_type'] ) {
+						$coupon->set_discount_type( $coupon_args['discount_type'] );
 					}
 
 					$coupon->set_amount( $value );
