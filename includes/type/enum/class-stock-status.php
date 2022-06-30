54bc4f4e28a2893e1e0b96fc646a413d572c4d7f
Matthijs Knigge <31346561+matthijs166@users.noreply.github.com>
2022-06-30 08:40:03 +0200

    feat: Add filter hook to stock status enum

--- includes/type/enum/class-stock-status.php
+++ includes/type/enum/class-stock-status.php
@@ -16,17 +16,27 @@ class Stock_Status {
 	 * Registers type
 	 */
 	public static function register() {
-		$values = [
-			'IN_STOCK'     => [ 'value' => 'instock' ],
-			'OUT_OF_STOCK' => [ 'value' => 'outofstock' ],
-			'ON_BACKORDER' => [ 'value' => 'onbackorder' ],
-		];
-
 		register_graphql_enum_type(
 			'StockStatusEnum',
 			[
 				'description' => __( 'Product stock status enumeration', 'wp-graphql-woocommerce' ),
-				'values'      => $values,
+				'values'      => self::get_stock_statuses(),
+			]
+		);
+	}
+
+	/**
+	 * Returns WooCommerce stock status values to be exposed to the GraphQL schema.
+	 *
+	 * @return array
+	 */
+	private static function get_stock_statuses() {
+		return apply_filters(
+			'graphql_woocommerce_product_stock_statuses',
+			[
+				'IN_STOCK'     => [ 'value' => 'instock' ],
+				'OUT_OF_STOCK' => [ 'value' => 'outofstock' ],
+				'ON_BACKORDER' => [ 'value' => 'onbackorder' ],
 			]
 		);
 	}
