6c1a37b53e0acd95b5887f51f8743c578cad502d
Jason Bahl <jasonbahl@mac.com>
2022-11-30 10:13:23 -0700

    - update class-customers to return arrays for edges and nodes - move id resolver from product-attribute interface to the specific types

--- includes/type/interface/class-product-attribute.php
+++ includes/type/interface/class-product-attribute.php
@@ -46,9 +46,6 @@ class Product_Attribute {
 			'id'          => [
 				'type'        => [ 'non_null' => 'ID' ],
 				'description' => __( 'Attribute Global ID', 'wp-graphql-woocommerce' ),
-				'resolve'     => function ( $attribute ) {
-					return ! empty( $attribute->_relay_id ) ? $attribute->_relay_id : null;
-				},
 			],
 			'attributeId' => [
 				'type'        => [ 'non_null' => 'Int' ],
