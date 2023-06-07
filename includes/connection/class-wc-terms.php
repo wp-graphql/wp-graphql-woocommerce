fe29ce1d802c8a392757ea1599f4d91cfc7a7dc7
David Levine <david@axepress.dev>
2023-06-07 06:30:17 +0000

    chore: simplify conditional logic in WC_Terms::register_connections()

--- includes/connection/class-wc-terms.php
+++ includes/connection/class-wc-terms.php
@@ -28,47 +28,46 @@ class WC_Terms extends TermObjects {
 	 * Registers the various connections from other Types to WooCommerce taxonomies.
 	 */
 	public static function register_connections() {
-		$allowed_taxonomies = WPGraphQL::get_allowed_taxonomies();
+		/** @var array<string,\WP_Taxonomy> $allowed_taxonomies */
+		$allowed_taxonomies = WPGraphQL::get_allowed_taxonomies( 'objects' );
 		$wc_post_types      = WP_GraphQL_WooCommerce::get_post_types();
 
 		// Loop through the allowed_taxonomies to register appropriate connections.
-		if ( ! empty( $allowed_taxonomies && is_array( $allowed_taxonomies ) ) ) {
-			foreach ( $allowed_taxonomies as $taxonomy ) {
-				$tax_object = get_taxonomy( $taxonomy );
+		foreach ( $allowed_taxonomies as $taxonomy => $tax_object ) {
+			foreach ( $wc_post_types as $post_type ) {
+				if ( 'product' === $post_type ) {
+					continue;
+				}
 
-				// Registers the connections between each allowed PostObjectType and it's TermObjects.
-				if ( ! empty( $wc_post_types ) && is_array( $wc_post_types ) ) {
-					foreach ( $wc_post_types as $post_type ) {
-						if ( 'product' === $post_type ) {
-							continue;
-						}
+				if ( in_array( $post_type, $tax_object->object_type, true ) ) {
+					$post_type_object = get_post_type_object( $post_type );
 
-						if ( in_array( $post_type, $tax_object->object_type, true ) ) {
-							$post_type_object = get_post_type_object( $post_type );
-							register_graphql_connection(
-								self::get_connection_config(
-									$tax_object,
-									[
-										'fromType'      => $post_type_object->graphql_single_name,
-										'toType'        => $tax_object->graphql_single_name,
-										'fromFieldName' => $tax_object->graphql_plural_name,
-										'resolve'       => function( $source, array $args, AppContext $context, ResolveInfo $info ) use ( $tax_object ) {
-											$resolver = new TermObjectConnectionResolver( $source, $args, $context, $info, $tax_object->name );
+					if ( null === $post_type_object ) {
+						continue;
+					}
 
-											$term_ids = \wc_get_object_terms( $source->ID, $tax_object->name, 'term_id' );
+					register_graphql_connection(
+						self::get_connection_config(
+							$tax_object,
+							[
+								'fromType'      => $post_type_object->graphql_single_name,
+								'toType'        => $tax_object->graphql_single_name,
+								'fromFieldName' => $tax_object->graphql_plural_name,
+								'resolve'       => function( $source, array $args, AppContext $context, ResolveInfo $info ) use ( $tax_object ) {
+									$resolver = new TermObjectConnectionResolver( $source, $args, $context, $info, $tax_object->name );
 
-											$resolver->set_query_arg( 'term_taxonomy_id', ! empty( $term_ids ) ? $term_ids : [ '0' ] );
+									$term_ids = \wc_get_object_terms( $source->ID, $tax_object->name, 'term_id' );
 
-											return $resolver->get_connection();
-										},
-									]
-								)
-							);
-						}//end if
-					}//end foreach
+									$resolver->set_query_arg( 'term_taxonomy_id', ! empty( $term_ids ) ? $term_ids : [ '0' ] );
+
+									return $resolver->get_connection();
+								},
+							]
+						)
+					);
 				}//end if
 			}//end foreach
-		}//end if
+		}//end foreach
 
 		// From Coupons to ProductCategory connections.
 		$tax_object = get_taxonomy( 'product_cat' );
