4502bcd1a45de1a72ff00903e97c071a210faad9
Dovid Levine <david@axepress.dev>
2023-07-19 23:12:36 +0300

    fix: initialize arrays before using (#765)
    * fix!: update deps to required versions
    
    * fix; initialize arrays before using

--- includes/data/connection/class-customer-connection-resolver.php
+++ includes/data/connection/class-customer-connection-resolver.php
@@ -49,7 +49,8 @@ class Customer_Connection_Resolver extends AbstractConnectionResolver {
 		/**
 		 * Prepare for later use
 		 */
-		$last = ! empty( $this->args['last'] ) ? $this->args['last'] : null;
+		$last       = ! empty( $this->args['last'] ) ? $this->args['last'] : null;
+		$query_args = [];
 
 		/**
 		 * Set the $query_args based on various defaults and primary input $args
