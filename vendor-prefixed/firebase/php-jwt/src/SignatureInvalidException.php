9318afcc27b78df34810228293b51ee584af9a28
Geoff Taylor <geoff@axistaylor.com>
2023-08-04 20:56:53 -0400

    fix: PHP-JWT versioned to maintain support for composer and WP Bedrock installations

---
+++ vendor-prefixed/firebase/php-jwt/src/SignatureInvalidException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @license BSD-3-Clause
+ *
+ * Modified by Geoff Taylor using Strauss.
+ * @see https://github.com/BrianHenryIE/strauss
+ */
+
+namespace WPGraphQL\WooCommerce\Vendor\Firebase\JWT;
+
+class SignatureInvalidException extends \UnexpectedValueException
+{
+}
