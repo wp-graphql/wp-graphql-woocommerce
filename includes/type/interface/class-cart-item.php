ac5fa6b9ab8f19ffbf1ae8be43834abdf05a2c69
Geoffrey K Taylor <geoff@axistaylor.com>
2023-09-18 13:55:00 -0400

    chore: version numbers set and readmes updated. (#805)

--- includes/type/interface/class-cart-item.php
+++ includes/type/interface/class-cart-item.php
@@ -3,7 +3,7 @@
  * WPInterface Type - CartItem
  *
  * @package WPGraphQL\WooCommerce\Type\WPInterface
- * @since   TBD
+ * @since   0.17.0
  */
 
 namespace WPGraphQL\WooCommerce\Type\WPInterface;
--- includes/type/interface/class-downloadable-product.php
+++ includes/type/interface/class-downloadable-product.php
@@ -3,7 +3,7 @@
  * Defines the "DownloadableProduct" interface.
  * 
  * @package WPGraphQL\WooCommerce\Type\WPInterface
- * @since   TBD
+ * @since   0.17.0
  */
 
 namespace WPGraphQL\WooCommerce\Type\WPInterface;
