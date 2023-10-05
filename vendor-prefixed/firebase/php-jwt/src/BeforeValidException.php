d0db73af67d8695bd4b431b346df161158b597d3
Geoffrey K Taylor <geoff@axistaylor.com>
2023-10-05 01:21:39 -0400

    fix: paymentMethod fields no longer throw for guest users (#809)
    * fix: paymentMethod fields no longer throw for guest users
    
    * chore: CollectionStatsQueryTest reverted

--- vendor-prefixed/firebase/php-jwt/src/BeforeValidException.php
+++ vendor-prefixed/firebase/php-jwt/src/BeforeValidException.php
@@ -8,6 +8,17 @@
 
 namespace WPGraphQL\WooCommerce\Vendor\Firebase\JWT;
 
-class BeforeValidException extends \UnexpectedValueException
+class BeforeValidException extends \UnexpectedValueException implements JWTExceptionWithPayloadInterface
 {
+    private object $payload;
+
+    public function setPayload(object $payload): void
+    {
+        $this->payload = $payload;
+    }
+
+    public function getPayload(): object
+    {
+        return $this->payload;
+    }
 }
