51823e52740e376ee12b212b4bbcaf21b0f6aa7b
Geoff Taylor <geoff@axistaylor.com>
2023-10-05 00:47:34 -0400

    chore: CollectionStatsQueryTest reverted

--- tests/wpunit/CollectionStatsQueryTest.php
+++ tests/wpunit/CollectionStatsQueryTest.php
@@ -67,7 +67,7 @@ class CollectionStatsQueryTest extends \Tests\WPGraphQL\WooCommerce\TestCase\Woo
                 'collectionStats.attributeCounts', 
                 [
                     $this->expectedField('slug', 'PA_COLOR' ),
-                    $this->expectedField('label', 'pa_color' ),
+                    $this->expectedField('label', 'color' ),
                     $this->expectedField('name', 'color' ),
                     $this->expectedNode(
                         'terms',
