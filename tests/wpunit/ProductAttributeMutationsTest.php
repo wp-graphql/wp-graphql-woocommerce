10dda1047f8790fa04d461cbf8a4fde609ae2c96
Geoff Taylor <geoff@axistaylor.com>
2024-05-07 13:56:02 -0400

    devops: Product and product attribute mutation tests generated

---
+++ tests/wpunit/ProductAttributeMutationsTest.php
@@ -0,0 +1,15 @@
+<?php
+
+class ProductAttributeMutationsTest extends \Tests\WPGraphQL\WooCommerce\TestCase\WooGraphQLTestCase {
+    public function testCreateProductAttribute() {
+        $this->markTestIncomplete();
+    }
+
+    public function testUpdateProductAttribute() {
+        $this->markTestIncomplete();
+    }
+
+    public function testDeleteProductAttribute() {
+        $this->markTestIncomplete();
+    }
+}
---
+++ tests/wpunit/ProductMutationsTest.php
@@ -0,0 +1,15 @@
+<?php
+
+class ProductMutationsTest extends \Tests\WPGraphQL\WooCommerce\TestCase\WooGraphQLTestCase {
+    public function testCreateProduct() {
+        $this->markTestIncomplete();
+    }
+
+    public function testUpdateProduct() {
+        $this->markTestIncomplete();
+    }
+
+    public function testDeleteProduct() {
+        $this->markTestIncomplete();
+    }
+}
