9e01c9c77f9e5084da8efa992b089cf74b05eb7e
Alex Lykesas <alexookah@me.com>
2024-07-17 00:03:33 +0300

    Add Product_Attributes_Connection_Orderby_Enum with MENU_ORDER

--- includes/class-type-registry.php
+++ includes/class-type-registry.php
@@ -48,6 +48,7 @@ class Type_Registry {
 		Type\WPEnum\Currency_Enum::register();
 		Type\WPEnum\Shipping_Location_Type_Enum::register();
 		Type\WPEnum\WC_Setting_Type_Enum::register();
+		Type\WPEnum\Product_Attributes_Connection_Orderby_Enum::register();
 
 		/**
 		 * InputObjects.
