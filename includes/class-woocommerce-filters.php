83f50c3a30d9e2f166fe120be989212c397b383d
Alex Lykesas <alexookah@me.com>
2024-07-29 00:26:44 +0300

    Improve add_filter functions

--- includes/class-woocommerce-filters.php
+++ includes/class-woocommerce-filters.php
@@ -167,19 +167,8 @@ class WooCommerce_Filters {
 	public static function get_reset_password_message( $message, $key, $user_login, $user_data ) {
 		$wc_reset_email = \WC()->mailer()->emails['WC_Email_Customer_Reset_Password'];
 		if ( $wc_reset_email->is_enabled() ) {
-			add_filter(
-				'retrieve_password_title',
-				static function () use ( $wc_reset_email ) {
-					return $wc_reset_email->get_subject();
-				}
-			);
-
-			add_filter(
-				'wp_mail_content_type',
-				static function () use ( $wc_reset_email ) {
-					return $wc_reset_email->get_content_type();
-				}
-			);
+			add_filter( 'retrieve_password_title', [ $wc_reset_email, 'get_subject' ] );
+			add_filter( 'wp_mail_content_type', [ $wc_reset_email, 'get_content_type' ] );
 
 			$wc_reset_email->user_login = $user_login;
 			$wc_reset_email->reset_key  = $key;
